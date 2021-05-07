d9cac361c1f3a3d240a4e849d8f2b4ae48fb2f1f
Circuit-Overtime <74301576+Circuit-Overtime@users.noreply.github.com>
2021-05-07 17:59:18 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,48 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+  getRank()
+  {
+     database.ref('Rank').on("value", (ranknum)=>{
+       this.rank = ranknum.val();
+     })
+
+  }
+  static updateRank(rankcount)
+  {
+    database.ref('/').update({
+      Rank: rankcount
+    });
+  }
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
